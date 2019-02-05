174ce2a54e0e037cde4bf88fae05baa281756e3d
Friedrich <Friedrich.Preusser@t-online.de>
2019-02-05 12:03:10 +0100

    update path

--- Image_analysis/R/load_data_and_metadata.R
+++ Image_analysis/R/load_data_and_metadata.R
@@ -1,66 +1,6 @@
-library("plyr")
-library("dplyr")
-library("ggplot2")
-library("rmarkdown")
-library("knitr")
-library("lubridate")
-library("wesanderson")
-library("readr")
-library("purrr")
-library("viridis")
-library("png")
-library("grid")
-library("ggpubr")
-library("RColorBrewer")
-library("shiny")
-library("shinyWidgets")
 
-theme_black = function(base_size = 12, base_family = "") {
-  
-  theme_classic(base_size = base_size, base_family = base_family) %+replace%
-    
-    theme(
-      # Specify axis options
-      axis.line = element_line(colour = "white"),  
-      axis.text.x = element_text(size = base_size, color = "white", lineheight = 0.9),  
-      axis.text.y = element_text(size = base_size, color = "white", lineheight = 0.9),  
-      axis.ticks = element_line(color = "white", size  =  0.2),  
-      axis.title.x = element_text(size = base_size, color = "white", margin = margin(0, 10, 0, 0)),  
-      axis.title.y = element_text(size = base_size, color = "white", angle = 90, margin = margin(0, 10, 0, 0)),  
-      axis.ticks.length = unit(0.3, "lines"),   
-      # Specify legend options
-      legend.background = element_rect(color = NA, fill = "black"),  
-      legend.key = element_rect(color = "white",  fill = "black"),  
-      legend.key.size = unit(1.2, "lines"),  
-      legend.key.height = NULL,  
-      legend.key.width = NULL,      
-      legend.text = element_text(size = base_size*0.8, color = "white"),  
-      legend.title = element_text(size = base_size*0.8, face = "bold", hjust = 0, color = "white"),  
-      legend.position = "right",  
-      legend.text.align = NULL,  
-      legend.title.align = NULL,  
-      legend.direction = "vertical",  
-      legend.box = NULL, 
-      # Specify panel options
-      panel.background = element_rect(fill = "black", color  =  NA),  
-      panel.border = element_blank(),  
-      panel.grid.major = element_blank(),  
-      panel.grid.minor = element_blank(),  
-      panel.spacing = unit(0.5, "lines"),   
-      # Specify facetting options
-      strip.background = element_blank(), #(fill = "grey30", color = "grey10"),  
-      strip.text.x = element_text(size = base_size*0.8, color = "white", face = "italic"),  
-      strip.text.y = element_text(size = base_size*0.8, color = "white",angle = -90),  
-      # Specify plot options
-      plot.background = element_rect(color = "black", fill = "black"),  
-      plot.title = element_text(size = base_size*1.2, color = "white"),  
-      plot.margin = unit(rep(1, 4), "lines")
-      
-    )
-  
-}
 
-target_folder_general <- c("/media/ella/raid5/timelapses/2018-12-07_16-00-07")
+target_folder_general <- c("/media/ella/raid5/timelapses/2019-01-22_15-48-35")
 target_folder_analysis <- file.path(target_folder_general, "analysis")
 target_folder_analysis_csvs <- list.files(target_folder_analysis,pattern=".+\\.csv", full.names = TRUE)
 
@@ -72,7 +12,7 @@ metadata_file <- list.files(target_folder_general,pattern=".+\\_metadata.txt", f
 metadata <- data.frame(sapply(metadata_file, function (x) readLines(x, warn=FALSE)), stringsAsFactors = FALSE)
 colnames(metadata) <- as.character(gsub(".+\\/([0-9]+\\-[0-9]+\\-[0-9]+\\_[0-9]+\\-[0-9]+\\-[0-9]+)\\/.+", "\\1",target_folder_analysis))
 
-time_elapsed <- apply(metadata,2, function(x) as.numeric(gsub(".+\\s([0-9]+).*", "\\1",x[grepl("Time elpased.+\\:\\s(.+)",x)])))
+time_elapsed <- apply(metadata,2, function(x) as.numeric(gsub(".+\\s([0-9]+).*", "\\1",x[grepl("Time elapsed.+\\:\\s(.+)",x)])))
 time_elapsed <- as.data.frame(time_elapsed)
 time_elapsed$dataset <- gsub(".+\\/([0-9]+\\-[0-9]+\\-[0-9]+\\_[0-9]+\\-[0-9]+\\-[0-9]+)\\/.+", "\\1",rownames(time_elapsed))
 
